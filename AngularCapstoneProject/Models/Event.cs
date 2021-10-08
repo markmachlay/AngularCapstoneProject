90149a3fd95630208328af4653991e2813b7cd09
markmachlay <mblmachlay@gmail.com>
2021-10-07 20:48:19 -0400

    Blah

---
+++ AngularCapstoneProject/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AngularCapstoneProject.Models
+{
+    public partial class Event
+    {
+        public Event()
+        {
+            Favorites = new HashSet<Favorite>();
+        }
+
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public DateTime? EventDate { get; set; }
+        public decimal? Admission { get; set; }
+
+        public virtual ICollection<Favorite> Favorites { get; set; }
+    }
+}
