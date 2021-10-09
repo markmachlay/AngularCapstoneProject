870e250425de245a245df42a5633a5d7391b2912
Morgan Bateman <mbateman855@gmail.com>
2021-10-09 17:54:54 -0400

    added a component and added navbar buttons

--- AngularCapstoneProject/ClientApp/src/app/app.module.ts
+++ AngularCapstoneProject/ClientApp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { EventsComponent } from './events/events.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { PerformersComponent } from './performers/performers.component';
+import { AddEventComponent } from './add-event/add-event.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { PerformersComponent } from './performers/performers.component';
     FetchDataComponent,
     EventsComponent,
     FavoritesComponent,
-    PerformersComponent
+    PerformersComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -33,6 +35,7 @@ import { PerformersComponent } from './performers/performers.component';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'events', component: EventsComponent },
+      { path: 'add-event', component: AddEventComponent },
     ])
   ],
   providers: [],
