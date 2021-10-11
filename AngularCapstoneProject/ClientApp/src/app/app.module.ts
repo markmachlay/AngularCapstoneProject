741b592ac90bab5e970adefe12a1da3491f96009
markmachlay <mblmachlay@gmail.com>
2021-10-10 20:03:27 -0400

    Add event is wired in the nav bar

--- AngularCapstoneProject/ClientApp/src/app/app.module.ts
+++ AngularCapstoneProject/ClientApp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { EventsComponent } from './events/events.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { PerformersComponent } from './performers/performers.component';
+import { AddEventComponent } from './add-event/add-event.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { PerformersComponent } from './performers/performers.component';
     FetchDataComponent,
     EventsComponent,
     FavoritesComponent,
-    PerformersComponent
+    PerformersComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -32,7 +34,8 @@ import { PerformersComponent } from './performers/performers.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'events', component: EventsComponent }
+      { path: 'events', component: EventsComponent },
+      { path: 'add-event', component: AddEventComponent }
     ])
   ],
   providers: [],
