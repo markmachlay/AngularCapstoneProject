0a96eec1fd9ba4cd665eb3e280ebb8b074f2f9d2
markmachlay <mblmachlay@gmail.com>
2021-10-11 22:32:07 -0400

    Added Event Details in the Event Model and added User model, working on Favorite model

---
+++ AngularCapstoneProject/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AngularCapstoneProject.Models
+{
+    public partial class Users
+    {
+        public Users()
+        {
+            Favorites = new HashSet<Favorite>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+
+        public virtual ICollection<Favorite> Favorites { get; set; }
+    }
+}
